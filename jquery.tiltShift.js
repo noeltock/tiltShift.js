4fe21783e6a461aa36415e98a8e0b7fe9fa70343
Adam Yanalunas <adam@yanalunas.com>
2012-09-07 08:41:50 -0700

    Separated jQuery plugin from core code. Added AMD support. Updated logic so objects representing tilted items are returned so adjustments can be made without re-tilting. Moved all variable declarations to top of respective functions. Added option for tilt settings to be declared at instantiation time instead of written into element attributes. Hacked in workaround for define not being available.

--- jquery.tiltShift.js
+++ jquery.tiltShift.js
@@ -8,82 +8,154 @@
 
  */
 
-(function($) {
+// Workaround for 'use strict' mode
 
-    $.fn.tiltShift = function(options) {
-        $(this).each(function(){
+var define = define || undefined;
 
-            // Settings
+(function(window, $, define, undefined) {
 
-            var $this = $(this);
-            var $parent = $this.parent();
-            var s_position = $this.data('position');
-            var s_blur = $this.data('blur');
-            var s_focus = $this.data('focus');
-            var s_falloff = $this.data('falloff');
-            var s_direction = $this.data('direction');
+    'use strict';
+
+    var TiltShift = function(el, settings) {
+        this.$el = $(el);
+        this.settings = settings;
+        this.$wrap = null;
+
+        return this;
+    };
+
+    TiltShift.prototype.tilt = function(options) {
+        // Settings
+
+        options || ( options = this.$el.data() );
+
+        var $this = this.$el,
+            $parent = $this.parent(),
+            s_position = options.position || $this.data('position'),
+            s_blur =  options.blur || $this.data('blur'),
+            s_focus = options.focus || $this.data('focus'),
+            s_falloff = options.falloff || $this.data('falloff'),
+            s_direction = ( options.direction || $this.data('direction') ).toLowerCase(),
+            beforeEnd,
+            afterEnd,
+            beforeFall,
+            afterFall,
+            beforeDirection,
+            afterDirection,
+            angle;
+
+        // Don't re-shift
+
+        if( !this.$wrap && !$parent.hasClass('tiltshift-wrap') ) {
 
             // Setup DOM around Image (ugly but flexible)
 
-            var $wrap = $this.wrap('<div class="tiltshift-wrap" />').parent();
-            $wrap.prepend('<div class="tiltshift-before tiltshift-layer" />');
-            $wrap.append('<div class="tiltshift-after tiltshift-layer" />');
+            this.$wrap = this.wrap();
 
             // Grab original image and assign to before & after
 
-            var src = $this.attr('src');
-            $parent.find('.tiltshift-layer').css('background-image', 'url(' + src + ')');
+            $parent.find('.tiltshift-layer').css('background-image', 'url(' + $this.attr('src') + ')');
 
             // Set Blur
 
             $parent.find('.tiltshift-layer').css({
-                '-webkit-filter': 'blur(' + s_blur + 'px) contrast(125%) saturate(105%)'
+                '-webkit-filter': 'blur(' + s_blur + 'px) contrast(115%) saturate(103%)'
             });
+        }
 
-            // Calculate Focus Boundaries (impacted by inFocus value)
+        // Calculate Focus Boundaries (impacted by inFocus value)
 
-            var beforeEnd = ( s_position - ( s_focus / 2 ) ) / 100;
-            var afterEnd = ( ( 100 - s_position ) - ( s_focus / 2 ) ) / 100;
+        beforeEnd = ( s_position - ( s_focus / 2 ) ) / 100;
+        afterEnd = ( ( 100 - s_position ) - ( s_focus / 2 ) ) / 100;
 
-            // Calculate Falloff Breakpoints (impacted by tightness value)
+        // Calculate Falloff Breakpoints (impacted by tightness value)
 
-            var beforeFall = ( ( beforeEnd - ( s_falloff / 100 ) ) * 100 ).toFixed(2);
-            var afterFall = ( ( afterEnd - ( s_falloff / 100 ) ) * 100 ).toFixed(2);
+        beforeFall = ( ( beforeEnd - ( s_falloff / 100 ) ) * 100 ).toFixed(2);
+        afterFall = ( ( afterEnd - ( s_falloff / 100 ) ) * 100 ).toFixed(2);
 
-            // Adjust back up to integers for gradient format
+        // Adjust back up to integers for gradient format
 
-            beforeEnd *= 100;
-            afterEnd *= 100;
+        beforeEnd *= 100;
+        afterEnd *= 100;
 
-            // Set directional variables
+        // Set directional variables
 
-            var beforeDirection, afterDirection;
+        if ( s_direction === 'y' ) {
+            beforeDirection = '270deg';
+            afterDirection = '90deg';
+        } else if ( s_direction === 'x' ) {
+            beforeDirection = '180deg';
+            afterDirection = '0deg';
+        } else {
+            angle = s_direction % 360;
 
-            if ( s_direction === 'y' ) {
-                beforeDirection = '270deg';
-                afterDirection = '90deg';
-            } else if ( s_direction === 'x' ) {
-                beforeDirection = '180deg';
-                afterDirection = '0deg';
+            beforeDirection = (angle + 180) + 'deg';
+            afterDirection = angle + 'deg';
+        }
+
+        // Apply Gradient Mask to Image Layers
+
+        $parent.find('.tiltshift-before').css({
+            '-webkit-mask-image' : '-webkit-linear-gradient(' + beforeDirection + ', rgba(0,0,0,1) 0, rgba(0,0,0,1) ' + beforeFall + '%, rgba(0,0,0,0) ' + beforeEnd + '%)'
+        });
+
+        $parent.find('.tiltshift-after').css({
+            '-webkit-mask-image' : '-webkit-linear-gradient(' + afterDirection + ', rgba(0,0,0,1) 0, rgba(0,0,0,1) ' + afterFall + '%, rgba(0,0,0,0) ' + afterEnd + '%)'
+        });
+    };
+
+    TiltShift.prototype.untilt =  function() {
+        var $parent = this.$el.parent();
+
+        $parent.find('.tiltshift-layer').remove();
+
+        this.$el.unwrap();
+    };
+
+    TiltShift.prototype.wrap = function() {
+        var $wrap;
+
+        $wrap = this.$el.wrap('<div class="tiltshift-wrap" />').parent();
+        $wrap.prepend('<div class="tiltshift-before tiltshift-layer" />');
+        $wrap.append('<div class="tiltshift-after tiltshift-layer" />');
+
+        return $wrap;
+    };
+
+    window.TiltShift = TiltShift;
+
+    if ( define && typeof define === 'function' && define.amd ) {
+        define( 'tiltshift', [], function () { return TiltShift; } );
+    }
+})(window, jQuery, define );
+
+(function($, TiltShift) {
+
+    'use strict';
+
+    var cachedShifts = {};
+
+    $.fn.tiltShift = function(options) {
+        
+        var shifted = [];
+
+        $(this).each(function(){
+            var $this = $(this),
+                ts = (cachedShifts[this.src]) ? cachedShifts[this.src] : new TiltShift( $this, $this.data() );
+
+            if(options && options.reset) {
+                ts.untilt();
+                delete cachedShifts[this.src];
             } else {
-                var angle = s_direction % 360;
-
-                beforeDirection = (angle + 180) + 'deg';
-                afterDirection = angle + 'deg';
+                ts.tilt();
+                cachedShifts[this.src] = ts;
             }
 
-            // Apply Gradient Mask to Image Layers
-
-            $parent.find('.tiltshift-before').css({
-                '-webkit-mask-image' : '-webkit-linear-gradient(' + beforeDirection + ', rgba(0,0,0,1) 0, rgba(0,0,0,1) ' + beforeFall + '%, rgba(0,0,0,0) ' + beforeEnd + '%)'
-            });
-
-            $parent.find('.tiltshift-after').css({
-                '-webkit-mask-image' : '-webkit-linear-gradient(' + afterDirection + ', rgba(0,0,0,1) 0, rgba(0,0,0,1) ' + afterFall + '%, rgba(0,0,0,0) ' + afterEnd + '%)'
-            });
-
+            shifted.push( ts );
         });
+
+        return shifted;
 
     };
 
-})(jQuery);
+})(jQuery, TiltShift);
